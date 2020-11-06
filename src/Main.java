ad2b06ff55e072a8bb5e255496a37792253583f0
Alexander <zaporozhskih.sasha@yandex.ru>
2020-11-06 23:30:17 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int A;
-        int B;
         Scanner in = new Scanner(System.in);
-        A = in.nextInt();
-        B = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+
         System.out.println(A+B);
-        in.close();
     }
 }
